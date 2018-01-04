22aa02f5835da26d6845a17973e23b8eb84ffde3
Andrew Stec <astec@my.bcit.ca>
2018-01-04 13:10:44 -0800

    Fix for Teams Github repo creation query

--- app/controllers/github.controller.ts
+++ app/controllers/github.controller.ts
@@ -341,7 +341,7 @@ function createGithubReposForTeams(payload: any): Promise<any> {
   }
 
   function getTeamsToBuildForSelectedDeliv(course: ICourseDocument, deliv: IDeliverableDocument) {
-    return Team.find({courseId: course._id, deliverableId: deliv._id, 
+    return Team.find({courseId: course._id, deliverableIds: deliv._id, 
       $where: 'this.disbanded !== true', 'githubState.repo.url': ''})
       .populate({path: 'members deliverableId'})
       .exec()
